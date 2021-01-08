552249c78e28c779983642a5ad2cdec5af5efcba
diegocastillogz <diegorlando5@gmail.com>
2021-01-08 00:42:45 -0500

    eslint initial settings

--- middleware/.eslintrc.js
+++ middleware/.eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
-  },
-  env: {
-    es6: true,
-    node: true,
-  },
-  rules: {
-    "no-undef": "error",
-  },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
 };
