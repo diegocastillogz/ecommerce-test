c2625008daec068cfcb4e0753d5228262ab979a3
diegocastillogz <diegorlando5@gmail.com>
2021-01-09 21:01:55 -0500

    useGetData hook test

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,1 +1,4 @@
 import "@testing-library/jest-dom";
+import "regenerator-runtime/runtime";
+
+jest.setTimeout(30000);
---
+++ frontend/src/hooks/useGetData/__tests__/useGetData.test.js
@@ -0,0 +1,22 @@
+import useGetData from "../";
+import { act, renderHook } from "@testing-library/react-hooks";
+
+const headers = {
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+  method: "GET",
+};
+
+describe("useGetData hook test", () => {
+  test("it should request with a given url", async () => {
+    global.fetch = jest.fn();
+    await act(() => renderHook(() => useGetData("testurl")));
+
+    expect(global.fetch).toBeCalledWith(
+      "http://localhost:3000/apitesturl",
+      headers
+    );
+  });
+});
